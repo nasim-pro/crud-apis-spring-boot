410b4f611daea483dff66531cc5d9fac94705228
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-14 18:31:47 +0530

    feat(api): add createdAt and updatedAt timestamps, update User constructor

--- apis/src/main/java/com/crud/apis/model/User.java
+++ apis/src/main/java/com/crud/apis/model/User.java
@@ -1,24 +1,36 @@
 package com.crud.apis.model;
 
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Document(collection = "users")
 public class User {
 
     @Id
     private String id;
-
     private String name;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
     private int age;
+    @CreatedDate
+    private Instant createdAt;
+    @LastModifiedDate
+    private Instant updatedAt;
 
     public User() {}
 
-    public User(String name, String email, int age) {
+    public User(String name, String email, int age, String password) {
         this.name = name;
         this.email = email;
         this.age = age;
+        this.password = password;
     }
 
     public String getId() {
@@ -51,6 +63,14 @@ public class User {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getPassword(){
+        return this.password;
     }
 } 
     
