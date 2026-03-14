2e01d7a789c95cd887c424f3ba2b87bd7c60e711
Nasim Ahmad <khannasimahmad@gmail.com>
2026-03-14 18:32:30 +0530

    feat(api): add method to retrieve user by email from repository

--- apis/src/main/java/com/crud/apis/repository/UserRepository.java
+++ apis/src/main/java/com/crud/apis/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.crud.apis.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.crud.apis.model.User;
 
 public interface UserRepository extends MongoRepository<User, String> {
-
+    Optional<User> findByEmail(String email);
 }
